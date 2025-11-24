8b028bf092c6f7178b08f93028d4de30419403b7
bryan <bryanalvarado1904@gmail.com>
2025-11-23 19:34:02 -0600

    Agregar componente CharacterList

---
+++ components/CharacterList.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const CharacterList = () => {
+  return (
+    <View>
+      <Text>CharacterList</Text>
+    </View>
+  )
+}
