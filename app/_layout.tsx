479721df577d6de939461bed7782549b033b27ac
bryan <bryanalvarado1904@gmail.com>
2025-11-23 20:53:25 -0600

    Agregar CharacterProvider en RootLayout y reemplazar mensaje de texto por CharacterList en Index

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,3 +1,4 @@
+import { CharacterProvider } from '@/context/CharacterProvider'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { Stack } from 'expo-router'
 
@@ -6,7 +7,9 @@ const queryClient = new QueryClient()
 export default function RootLayout() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Stack />
+      <CharacterProvider>
+        <Stack />
+      </CharacterProvider>
     </QueryClientProvider>
   )
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,16 @@
-import { Text, View } from "react-native";
+import { CharacterList } from '@/components/CharacterList'
+import { View } from 'react-native'
 
 export default function Index() {
   return (
     <View
       style={{
         flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
+        justifyContent: 'center',
+        alignItems: 'center',
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <CharacterList />
     </View>
-  );
+  )
 }
